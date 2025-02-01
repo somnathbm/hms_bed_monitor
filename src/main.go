20fcf5264137894fb4d0d1c4b4131c0e0f6a927a
somnath <somnathbm0@gmail.com>
2025-02-01 08:13:19 +0530

    goenv removed

--- src/main.go
+++ src/main.go
@@ -3,16 +3,15 @@ package main
 import (
 	// "hospi_bed_stats/bed_stats"
 	"hospi_bed_stats/api"
-
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
 func main() {
 	// 	bed_num := bed_stats.GetBedStats()
 	// 	fmt.Printf("%v beds are available\n", bed_num)
-	err := godotenv.Load()
-	if err != nil {
+	// err := godotenv.Load()
+	// if err != nil {
 
-	}
+	// }
 	api.Api()
 }
